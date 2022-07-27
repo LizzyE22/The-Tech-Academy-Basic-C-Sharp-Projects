3402df89796395e162a8019f477b70fc19dec47a
Lizzy Esqueda <99945107+LizzyE22@users.noreply.github.com>
2022-07-27 15:51:38 -0700

    Add files via upload

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intro to daily report//
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            //The following are questions for the user//
+            Console.WriteLine("What is your name?");
+            string askName = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string askCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            int askPage = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? Please answer \"True\" or \"False\"");
+            bool askHelp = Convert.ToBoolean(Console.ReadLine());
+
+
+            Console.WriteLine("Were there any positive experiences you'd like to provide? Please be specific.");
+            string askExperience = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string askFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string askHours = Console.ReadLine();
+            int hoursAnswer = Convert.ToInt32(askHours);
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
