5313fd906de04491077f0ad5fa149727d72954f8
LizzyE22 <esqueda22@gmail.com>
2022-07-28 16:24:17 -0700

    edit

--- Branching_SubmissionProject/Branching_SubmissionProject/Program.cs
+++ Branching_SubmissionProject/Branching_SubmissionProject/Program.cs
@@ -10,6 +10,7 @@ namespace Branching_SubmissionProject
     {
         static void Main(string[] args)
         {
+            //User input for package weight to be less than 50 to continue//
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
             Console.WriteLine("Please enter the package weight");
             string askWeight = Console.ReadLine();
@@ -18,21 +19,24 @@ namespace Branching_SubmissionProject
             {
                 Console.WriteLine("Package to heavy to be shipped via Package Express. Have a good day.");
             }
+            //User input for package width//
             Console.WriteLine("Please enter the package width:");
             string askWidth = Console.ReadLine();
             decimal widthAnswer = Convert.ToDecimal(askWidth);
-
+            //User input for package height//
             Console.WriteLine("Please enter the package height:");
             string askHeight = Console.ReadLine();
             decimal heightAnswer = Convert.ToDecimal(askHeight);
-
+            //User input for package length//
             Console.WriteLine("Please enter the package length:");
             string askLength = Console.ReadLine();
             decimal lengthAnswer = Convert.ToDecimal(askLength);
+            //Calculating total weight of dimensions to be less than 50 to continue//
             if (widthAnswer + heightAnswer + lengthAnswer > 50)
             {
                 Console.WriteLine("Package too big to be shipped via Package Express.");
             }
+            //Multiplying dimensions by weight and dividing by 100 for total displayed in dollar amount//
             decimal totalDimension = widthAnswer * heightAnswer * lengthAnswer * weightAnswer / 100;
             //decimal dimensionAnswer = Convert.ToDecimal(totalDimension);
             //Console.WriteLine("$" + dimensionAnswer);
