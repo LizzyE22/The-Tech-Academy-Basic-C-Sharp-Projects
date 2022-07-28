aacb01dfca398bb8d529d11dfabd4a7db7573abc
LizzyE22 <esqueda22@gmail.com>
2022-07-27 23:17:20 -0700

    push

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////AND Operator examples//
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+
+            ////OR Operator examples//
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(true || true);
+
+            //Equals Operator examples//
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(true == true);
+
+            // Does not equal Operator examples//
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+
+            //XOR Operator examples; true if one is true but not both//
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+
+            Console.WriteLine(true && true && true && false);
+            Console.WriteLine(true && true && true || false);
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ BooleanLogic_SubmissionProject/BooleanLogic_SubmissionProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic_SubmissionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string askAge = Console.ReadLine();
+            int ageAnswer = Convert.ToInt32(askAge);
+
+            Console.WriteLine("Have you ever had a DUI? Answer True or False.");
+            string askDUI = Console.ReadLine();
+            bool duiAnswer = Convert.ToBoolean(askDUI);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string askTickets = Console.ReadLine();
+            int ticketAnswer = Convert.ToInt32(askTickets);
+
+            Console.WriteLine("Qualified?");
+            bool qualified = Convert.ToBoolean(ageAnswer > 15 && duiAnswer == false && ticketAnswer <= 3);
+            Console.WriteLine(qualified);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
