da28f0dc9a5e385105f840f5b96adf53bd358d1e
LizzyE22 <esqueda22@gmail.com>
2022-07-28 18:38:15 -0700

    update

---
+++ LoopsExercises/LoopsExercises/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+            //the "do"code is used if the user guesses the right answer(12)on the 1st try//
+            do
+            {
+                switch (number)
+                {
+                    //"if number equals 62..."//
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        //must put break or will keep going with switch statement//
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    //if no cases evaluate to true, use default//
+                    default:
+                        Console.WriteLine("You're wrong.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            //The below code (!isGuessed is the same as (isGuessed == false)//
+            while (!isGuessed);
+            
+                
+            
+            Console.Read();
+
+            
+        }
+    }
+}
