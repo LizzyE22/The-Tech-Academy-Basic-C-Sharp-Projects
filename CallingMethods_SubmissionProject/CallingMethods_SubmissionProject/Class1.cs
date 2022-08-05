72660b821f998fe503a994bb1e970749af811759
LizzyE22 <esqueda22@gmail.com>
2022-08-05 16:22:53 -0700

    push

---
+++ CallingMethods_SubmissionProject/CallingMethods_SubmissionProject/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods_SubmissionProject
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Num1 = 10;
+            Num2 = 20;
+
+        }
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+    }
+
+}
---
+++ CallingMethods_SubmissionProject/CallingMethods_SubmissionProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods_SubmissionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+         
+        {
+            Console.WriteLine("What number would you like to do Math with?");
+            string askNumber = Console.ReadLine();
+            int userNumber = Convert.ToInt32(askNumber);
+
+            Class1 class1 = new Class1();
+            //method
+            class1.Num1 = 10;
+            class1.Num2 = 20;
+
+            Console.WriteLine(class1.Num1 + class1.Num2);
+            Console.ReadLine();
+
+            class1.Num1 = 30;
+            class1.Num2 = 40;
+
+            Console.WriteLine(class1.Num1 * class1.Num2);
+            Console.ReadLine();
+
+            class1.Num1 = 60;
+            class1.Num2 = 20;
+            Console.WriteLine(class1.Num1 / class1.Num2);
+            Console.ReadLine();
+
+        }
+    }
+}
