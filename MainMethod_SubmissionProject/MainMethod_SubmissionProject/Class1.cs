66dbe0dcc64113c99796f98730939baed769c556
LizzyE22 <esqueda22@gmail.com>
2022-08-05 19:36:34 -0700

    update

---
+++ MainMethod_SubmissionProject/MainMethod_SubmissionProject/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod_SubmissionProject
+{
+    public class Class1
+    {
+        public int Addition(int Number)
+        {
+            int result = Number + 100;
+            return result;
+        }
+        public decimal Addition2(decimal Number)
+        {
+            decimal result = Number + 200;
+            return result;
+        }
+        public string Addition3(string Number)
+        {
+            string result = Number + 300;
+            string Addition3 = Convert.ToInt32(Number);
+
+            return result;
+        }
+      
+
+        
+    }
+}
---
+++ MainMethod_SubmissionProject/MainMethod_SubmissionProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod_SubmissionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            string askNumber = Console.ReadLine();
+            //string Answer3 = Convert.ToInt32((askNumber));
+
+            Class1 class1 = new Class1();
+
+            int Answer1 = class1.Addition(Number);
+            decimal Answer2 = class1.Addition2(Number);
+            int Answer3 = class1.Addition3(Number);
+
+            Console.WriteLine(Answer1);
+            Console.WriteLine(Answer2);
+            Console.WriteLine(Answer3);
+
+            Console.ReadLine();
+        }
+    }
+}
