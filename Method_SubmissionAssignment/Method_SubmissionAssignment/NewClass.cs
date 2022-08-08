5c70304bd0fa2be18d552114e80e88dae3d3183e
LizzyE22 <esqueda22@gmail.com>
2022-08-07 21:23:26 -0700

    push

---
+++ Method_SubmissionAssignment/Method_SubmissionAssignment/NewClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_SubmissionAssignment
+{
+    public class NewClass
+    {
+        //Calculate wine bottle price * 12 (case of wine)
+        public int Answer(int Number1, int Number2, int Number3 = 0)
+        {
+            int result = (Number1 + Number2 + Number3);
+            return result;
+        }
+    }
+}
+
+
+
---
+++ Method_SubmissionAssignment/Method_SubmissionAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_SubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //user input asking to enter numbers
+            Console.WriteLine("Enter two numbers");
+            int Number1 = Convert.ToInt32(Console.ReadLine());
+            int Number2 = Convert.ToInt32(Console.ReadLine());
+
+            //instantiating class
+            NewClass newclass = new NewClass();
+            int Total = newclass.Answer(Number1, Number2);
+
+            Console.WriteLine("Total: " + Total);
+            Console.ReadLine();
+        }
+    }
+}
