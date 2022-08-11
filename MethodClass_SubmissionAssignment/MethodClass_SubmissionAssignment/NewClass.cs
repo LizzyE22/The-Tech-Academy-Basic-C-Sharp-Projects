3f29921b52d9be977c16bece0c82453703e0e0af
LizzyE22 <esqueda22@gmail.com>
2022-08-10 18:24:25 -0700

    push

--- MethodClass_SubmissionAssignment/MethodClass_SubmissionAssignment/NewClass.cs
+++ MethodClass_SubmissionAssignment/MethodClass_SubmissionAssignment/NewClass.cs
@@ -8,9 +8,11 @@ namespace MethodClass_SubmissionAssignment
 {
     public class NewClass
     {
+        //creating a void method taking 2 integers as parameters
         public static void Answer(int Number1, int Number2)
 
         {
+            //math operation on 1st integer, and displaying 2nd integer to screen
             int result = (Number1 + 10);
             Console.WriteLine(Number2);
             Console.ReadLine();
--- MethodClass_SubmissionAssignment/MethodClass_SubmissionAssignment/Program.cs
+++ MethodClass_SubmissionAssignment/MethodClass_SubmissionAssignment/Program.cs
@@ -13,8 +13,11 @@ namespace MethodClass_SubmissionAssignment
             int Number1 = 2;
             int Number2 = 4;
 
+            //instantiating class
             NewClass newClass = new NewClass();
+            //calling class, passing in 2 numbers
             NewClass.Answer(2, 4);
+            //calling class specifying parameters by name
             NewClass.Answer(Number1, Number2);
         }           
     }
