3026d86890fe49725e6c30889f66a329ed9bd847
LizzyE22 <esqueda22@gmail.com>
2022-08-18 12:07:14 -0700

    push

---
+++ DateTime_Submission_Assignment/DateTime_Submission_Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Submission_Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+
+        {
+       
+            //public DateTime AddHours(double value);
+
+            DateTime dateTime = DateTime.Now;
+            //printing current date and time
+            Console.WriteLine(dateTime);
+            
+            //asking user input for number 
+            Console.WriteLine("Please enter a number");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            
+            //printing exact time it will be in hours with user input snumber 
+            DateTime newTime = dateTime.AddHours(userNumber);
+            Console.WriteLine(newTime);
+            Console.Read();
+        }
+    }
+}
