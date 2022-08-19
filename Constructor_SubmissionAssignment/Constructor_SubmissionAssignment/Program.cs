c12766500ea695897a84fff4a498411fb83520e4
LizzyE22 <esqueda22@gmail.com>
2022-08-18 17:08:22 -0700

    edit

--- Constructor_SubmissionAssignment/Constructor_SubmissionAssignment/Program.cs
+++ Constructor_SubmissionAssignment/Constructor_SubmissionAssignment/Program.cs
@@ -10,15 +10,15 @@ namespace Constructor_SubmissionAssignment
     {
         static void Main(string[] args)
         {
-            ////creating a constant variable
-            //const string wineChardonnay = "lemon notes, tree fruit including green apple, and great minerality";
-            //Console.WriteLine("This Chardonnay has {0}", wineChardonnay);
-            //Console.ReadLine();
+            //creating a constant variable
+            const string wineChardonnay = "lemon notes, tree fruit including green apple, and great minerality";
+            Console.WriteLine("This Chardonnay has {0}", wineChardonnay);
+            Console.ReadLine();
 
-            ////creating variable with keyword "var"
-            //var dic = new Dictionary<string, Dictionary<int, string>>();
+            //creating variable with keyword "var"
+            var dic = new Dictionary<string, Dictionary<int, string>>();
 
-            //chaining 2 contructors together example
+            chaining 2 contructors together example
             Employee newEmployee = new Employee("Lizzy");
             Console.WriteLine(newEmployee);
             Console.ReadLine();
