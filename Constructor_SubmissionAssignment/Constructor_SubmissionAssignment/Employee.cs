39043f0dd12be47f906f6bc95caf6b30ffcc57b5
LizzyE22 <esqueda22@gmail.com>
2022-08-18 17:06:18 -0700

    push

---
+++ Constructor_SubmissionAssignment/Constructor_SubmissionAssignment/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_SubmissionAssignment
+{
+    public class Employee
+    {
+        public int EmployeeWage { get; set; }
+        public string Name { get; set; }
+
+        //chaining 2 constructors together
+        public Employee(string name) : this(name, 25)
+        {
+
+        }
+        public Employee(string name, int wage)
+        {
+            EmployeeWage = wage;
+            Name = name;
+        }
+       
+    }
+}
---
+++ Constructor_SubmissionAssignment/Constructor_SubmissionAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_SubmissionAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ////creating a constant variable
+            //const string wineChardonnay = "lemon notes, tree fruit including green apple, and great minerality";
+            //Console.WriteLine("This Chardonnay has {0}", wineChardonnay);
+            //Console.ReadLine();
+
+            ////creating variable with keyword "var"
+            //var dic = new Dictionary<string, Dictionary<int, string>>();
+
+            //chaining 2 contructors together example
+            Employee newEmployee = new Employee("Lizzy");
+            Console.WriteLine(newEmployee);
+            Console.ReadLine();
+        }
+    }
+}
